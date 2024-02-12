e88db68607c57232ad3338db7a06b48da55dad7f
Letchkov Jean-Philippe <letch@Letchkovs-MacBook-Air.local>
2024-02-11 20:30:08 -0500

    done

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:27017/Thomas&Friends',{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
+
+
+module.exports = mongoose.connection
